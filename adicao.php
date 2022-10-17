674b4eb093669165308b36951dd291f394a69f97
geovannaruy2 <116040342+geovannaruy2@users.noreply.github.com>
2022-10-17 19:37:30 -0300

    Add files via upload

---
+++ adicao.php
@@ -0,0 +1,14 @@
+<?php
+
+$n1= $_POST['n1'];
+$n2= $_POST['n2'];
+
+$resultdo =$n1 =$n2;
+i( $resultado >20){
+    echo "A soma é : . " $resultado + 8;
+
+}
+else{
+    echo "A soma é : . " $resultado  - 5;
+}
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adição</title>
+
+</head>
+<body>
+ <form action="adicao.php" method="post">
+<div class="container">
+Insira o primeiro número: <input type="text" name="n1">
+<br>
+Insira o segundo número: <input type="text" name="n2">
+<br>
+<input type="button" value="Enviar">
+<input type="button" value="Cancelar">
+ </div>
+    </form>
+</body>
+</html>
